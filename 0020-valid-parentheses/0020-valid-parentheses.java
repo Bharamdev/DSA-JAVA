2e71659181fab1ebf8e3d0482808733230e9557e
Bharamdev <157497417+Bharamdev@users.noreply.github.com>
2025-01-24 23:54:54 +0530

    Time: 2 ms (97.63%), Space: 42 MB (33.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        while(s.contains("()") || s.contains("[]") || s.contains("{}") ){
-            s= s.replace("()", "").replace("[]", "").replace("{}", "");
+        Stack<Character> stack = new Stack();
+        for(char c: s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                if((c==')' && stack.peek()=='(' ) || (c==']' && stack.peek()=='[') || 
+                (c=='}' && stack.peek()=='{' )){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
         }
-        return s.isEmpty();
+        return stack.isEmpty();
     }
 }
