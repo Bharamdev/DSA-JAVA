d2069161158021b54777e3d450e4314f61451f2b
Bharamdev <157497417+Bharamdev@users.noreply.github.com>
2025-02-12 20:08:00 +0530

    Time: 1 ms (90.28%), Space: 45 MB (43.37%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -6,10 +6,10 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             prefix*=nums[i];
             suffix*=nums[n-1-i];
-            if(nums[i]==0) return 0;
+            max= Math.max(max, Math.max(suffix, prefix));
             if(prefix==0) prefix=1;
             if(suffix==0) suffix=1;
-            max= Math.max(max, Math.max(suffix, prefix));
+            
         }
         return max;
     }
