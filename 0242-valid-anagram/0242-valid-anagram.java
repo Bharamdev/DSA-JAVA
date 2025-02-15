46c8aa6448e2a438f1a02e49694f2f0bdc864fb2
Bharamdev <157497417+Bharamdev@users.noreply.github.com>
2025-02-15 14:55:49 +0530

    Time: 4 ms (75.6%), Space: 45.1 MB (7.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        HashMap<Character, Integer> mpp1 = new HashMap<>();
-        HashMap<Character, Integer> mpp2 = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-           mpp1.put(s.charAt(i), mpp1.getOrDefault(s.charAt(i),0) + 1);
-           mpp2.put(t.charAt(i), mpp2.getOrDefault(t.charAt(i),0) + 1);
-        }
-        return mpp1.equals(mpp2);
+        // HashMap<Character, Integer> mpp1 = new HashMap<>();
+        // HashMap<Character, Integer> mpp2 = new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //    mpp1.put(s.charAt(i), mpp1.getOrDefault(s.charAt(i),0) + 1);
+        //    mpp2.put(t.charAt(i), mpp2.getOrDefault(t.charAt(i),0) + 1);
+        // }
+        // return mpp1.equals(mpp2);
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        return Arrays.equals(s1,t1); 
     }
 }
