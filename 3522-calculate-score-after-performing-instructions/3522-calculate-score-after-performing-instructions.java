34f839627fec8ecae7c92a897e75d9da2f3f8483
Bharamdev <157497417+Bharamdev@users.noreply.github.com>
2025-04-25 17:18:13 +0530

    Time: 2 ms (97.87%), Space: 70 MB (65.3%) - LeetHub

---
+++ 3522-calculate-score-after-performing-instructions/3522-calculate-score-after-performing-instructions.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long calculateScore(String[] instructions, int[] values) {
+        int n=values.length;
+        int i=0;
+        long score=0;
+        // HashSet<Integer> set = new HashSet<>();
+        boolean[] check = new boolean[n];
+        while((i<n && i>=0) && !check[i]){
+            // set.add(i);
+            check[i]=true;
+            // if() return score;
+            if(instructions[i].equals("add")){
+                score+= values[i];
+                i++;
+            }
+            else{
+                i+=values[i];
+            }
+        }
+        return score;
+    }
+}
