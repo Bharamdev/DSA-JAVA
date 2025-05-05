3b173a25e18ee875d6883acf253f96c73050acf5
Bharamdev <157497417+Bharamdev@users.noreply.github.com>
2025-05-05 20:57:54 +0530

    Time: 0 ms (100%), Space: 41.5 MB (90.63%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -3,12 +3,16 @@ class Solution {
         int n=s.length();
         if(n<3) return 0;
         int cnt=0;
+        // for(int i=0;i<n-2;i++){
+        //     HashMap<Character,Integer> map = new HashMap<>();
+        //     map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        //     map.put(s.charAt(i+1),map.getOrDefault(s.charAt(i+1),0)+1);
+        //     map.put(s.charAt(i+2),map.getOrDefault(s.charAt(i+2),0)+1);
+        //     if(map.size()==3) cnt++;
+        // }
+        char[] arr = s.toCharArray();
         for(int i=0;i<n-2;i++){
-            HashMap<Character,Integer> map = new HashMap<>();
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-            map.put(s.charAt(i+1),map.getOrDefault(s.charAt(i+1),0)+1);
-            map.put(s.charAt(i+2),map.getOrDefault(s.charAt(i+2),0)+1);
-            if(map.size()==3) cnt++;
+            if(arr[i]!=arr[i+1] && arr[i+1]!=arr[i+2] && arr[i]!=arr[i+2]) cnt++;
         }
         return cnt;
     }
