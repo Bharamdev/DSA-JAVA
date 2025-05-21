060b53f4fa9288c2d8a74e000d2ea38051dd3143
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-05-22 00:24:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -0,0 +1,32 @@
+/* Structure of class Node is
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    // Function to check whether the list is palindrome.
+    static boolean isPalindrome(Node head) {
+        // Your code here
+        Node temp=head;
+        Stack<Integer> st = new Stack<>();
+        while(temp!=null){
+            st.push(temp.data);
+            temp=temp.next;
+        }
+        temp=head;
+        while(temp!=null){
+            if(temp.data!=st.peek()) return false;
+            st.pop();
+            temp=temp.next;
+        }
+        return true;
+    }
+}
