8474000a5bbdd3afff98105b0805f4b61edbf5c2
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-06-07 23:17:56 +0530

    Time: 3 ms (92.51%), Space: 46.8 MB (22.51%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,11 +3,23 @@ class Solution {
         int zero=0,max=0;
         int i=0,j=0;
         int n=nums.length;
+        // Worst time complexicity 2n
+        // while(j<n){
+        //     if(nums[j]==0){
+        //         zero++;
+        //     }
+        //     while(zero>k){
+        //         if(nums[i]==0) zero--;
+        //         i++;
+        //     }
+        //     if(zero<=k) max= Math.max(max,j-i+1);
+        //     j++;
+        // }
         while(j<n){
             if(nums[j]==0){
                 zero++;
             }
-            while(zero>k){
+            if(zero>k){
                 if(nums[i]==0) zero--;
                 i++;
             }
