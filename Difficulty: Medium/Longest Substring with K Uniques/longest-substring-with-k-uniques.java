c01427dd6098c83647512d4514e2108fd522c9d9
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-03 11:25:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -1,47 +1,23 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GfG {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            String s = sc.next();
-            int k = sc.nextInt();
-            Solution obj = new Solution();
-            System.out.println(obj.longestkSubstr(s, k));
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public int longestkSubstr(String s, int k) {
+    public int longestKSubstr(String s, int k) {
         // code here
-        HashMap<Character,Integer> map = new HashMap<>();
-        int j=0,maxLen=-1;
-        for(int i = 0; i < s.length(); i++){
-            char ch= s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            if(map.size()==k) maxLen=Math.max(maxLen, i-j+1);
-            
-            while(map.size()>k){
-                char start = s.charAt(j);
-                map.put(start,map.get(start)-1);
-                if(map.get(start)==0){
-                    map.remove(start);
-                }
-                j++;
+        Map<Character,Integer> map = new HashMap<>();
+        int len=-1;
+        int r=0,l=0;
+        while(r<s.length()){
+            char c = s.charAt(r);
+            map.put(c,map.getOrDefault(c,0)+1);
+            if(map.size()==k){
+                len=Math.max(len,r-l+1);
             }
+            while(map.size()>k){
+                char ch = s.charAt(l);
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch)<1) map.remove(ch);
+                l++;
+            }
+            r++;
         }
-        return maxLen;
+        return len;
     }
 }
