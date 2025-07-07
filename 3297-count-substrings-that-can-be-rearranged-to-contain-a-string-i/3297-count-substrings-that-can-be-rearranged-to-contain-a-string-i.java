b7a12224b0a21175f9dd714c6e41ff6ec64f1ff5
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-07 21:08:20 +0530

    Time: 357 ms (6.11%), Space: 45.5 MB (38.93%) - LeetHub

---
+++ 3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long validSubstringCount(String word1, String word2) {
+        int n=word1.length(), m=word2.length();
+        // int[] need = new int[26];
+        Map<Character,Integer> need = new HashMap<>();
+        for(char ch: word2.toCharArray()) need.put(ch, need.getOrDefault(ch,0)+1);
+        int size=need.size();
+        // int[] check = new int[26];
+        Map<Character,Integer> map = new HashMap<>();
+        int l=0;
+        long cnt=0;
+        for(int r=0;r<n;r++){
+            char ch = word1.charAt(r);
+            map.put(ch, map.getOrDefault(ch,0)+1);
+            while(map.size()>=size && isValid(map,need)){
+                char c=word1.charAt(l);
+                cnt+=n-r;
+                map.put(c,map.get(c)-1);
+                if(map.get(c)==0) map.remove(c);
+                l++;
+            }
+        }
+            return cnt;
+    }
+    public boolean isValid(Map<Character,Integer> map, Map<Character,Integer> need){
+        for(char key: need.keySet()){
+            int check = map.getOrDefault(key,0); 
+            if(check < need.get(key)) return false;
+        }
+        return true;
+    }
+}
