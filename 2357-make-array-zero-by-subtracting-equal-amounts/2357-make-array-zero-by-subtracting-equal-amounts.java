5a4b7bbfd280769b6e31c51291f4c5ce2d4c9627
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-08 18:47:10 +0530

    Time: 1 ms (95.05%), Space: 41.7 MB (21.88%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,9 +1,12 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        // Set<Integer> set = new HashSet<>();
+        Map<Integer,Integer> map = new HashMap<>();
         for(int num:nums){
-            if(num>0) set.add(num);
+            // if(num>0) set.add(num);
+            if(num>0) map.put(num, map.getOrDefault(num,0)+1);
         }
-        return set.size();
+        // return set.size();
+        return map.size();
     }
 }
