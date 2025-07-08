575cd8206506224a76e6ac0ee6f948f1e06f29f0
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-08 15:16:34 +0530

    Time: 21 ms (91.78%), Space: 45.8 MB (64.24%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -2,9 +2,10 @@ class Solution {
     public int distributeCandies(int[] candyType) {
         int n=candyType.length;
         HashSet<Integer> set = new HashSet<>();
-        for(int num: candyType) set.add(num);
-        int size= set.size();
-        if(size>=n/2) return n/2;
-        return size;
+        for(int num: candyType) {
+            if(set.size()==n/2) return n/2;
+            set.add(num);
+        }
+        return set.size();
     }
 }
