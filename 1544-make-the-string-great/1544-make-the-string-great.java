bcef8ae99e2fdf206897b44f0db83a4cd9f7cc2f
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-09 18:31:55 +0530

    Time: 2 ms (80.72%), Space: 41.9 MB (97.47%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,17 +1,25 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> st = new Stack<>();
+        // Stack<Character> st = new Stack<>();
         StringBuilder res = new StringBuilder();
         int n=s.length();
+        // for(int i=0;i<n;i++){
+        //     char ch=s.charAt(i);
+        //     if(!st.isEmpty() && Math.abs(st.peek()-ch)==32) st.pop();
+        //     else st.push(ch);
+        // }
+        // int size=st.size();
+        // for(int i=0;i<size;i++){
+        //     res.append(st.pop());
+        // }
+        // return res.reverse().toString();
+
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            if(!st.isEmpty() && Math.abs(st.peek()-ch)==32) st.pop();
-            else st.push(ch);
+            int len=res.length();
+            if(len>0 && Math.abs(res.charAt(len-1)-ch)==32) res.deleteCharAt(len-1);
+            else res.append(ch);
         }
-        int size=st.size();
-        for(int i=0;i<size;i++){
-            res.append(st.pop());
-        }
-        return res.reverse().toString();
+        return res.toString();
     }
 }
