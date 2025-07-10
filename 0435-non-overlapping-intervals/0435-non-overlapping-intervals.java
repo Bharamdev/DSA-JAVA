a0d0717be618e95691a54d6c976d4c368f36c62a
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-10 16:58:00 +0530

    Time: 48 ms (56.65%), Space: 73.3 MB (82.25%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,17 +1,33 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) ->{
-        if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
-        else return Integer.compare(a[1],b[1]);
-        });
-        int remove=0;
+        // Arrays.sort(intervals, (a,b) ->{
+        // if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
+        // else return Integer.compare(a[1],b[1]);
+        // });
+        // int remove=0;
+        // int n=intervals.length;
+        // if(n==1) return 0;
+        // int[] nonOverLapping = intervals[0];
+        // for(int i=1;i<n;i++){
+        //     if(nonOverLapping[1]>intervals[i][0]) {
+        //         remove++;
+        //         if(nonOverLapping[1]>intervals[i][1]) nonOverLapping=intervals[i];
+        //     }
+        //     else{
+        //         nonOverLapping= intervals[i];
+        //     }
+        // }
+        
+        // return remove;
+
+        // Sort intervals by end time (not start time)
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1],b[1]));
+         int remove=0;
         int n=intervals.length;
-        if(n==1) return 0;
         int[] nonOverLapping = intervals[0];
         for(int i=1;i<n;i++){
             if(nonOverLapping[1]>intervals[i][0]) {
                 remove++;
-                if(nonOverLapping[1]>intervals[i][1]) nonOverLapping=intervals[i];
             }
             else{
                 nonOverLapping= intervals[i];
