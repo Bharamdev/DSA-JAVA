b815e87a027f62740a8071393d842ab6db105f55
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-12 22:24:53 +0530

    Time: 56 ms (88.35%), Space: 42.9 MB (45.11%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,17 +1,34 @@
 class Solution {
     public int beautySum(String s) {
         // Map<Character,Integer> map = new HashMap<>();
-        int cnt=0;
-        // int min=501,max=0;
+        // int cnt=0;
+        // for(int i=0;i<s.length();i++){
+        //     Map<Character,Integer> map = new HashMap<>();
+        //     for(int j=i;j<s.length();j++){
+        //         char ch = s.charAt(j);
+        //         map.put(ch,map.getOrDefault(ch,0)+1);
+        //         int min=501,max=0;
+        //         for(int freq: map.values()){
+        //             max=Math.max(max,freq);
+        //             min = Math.min(min,freq);
+        //         }
+        //         cnt+=max-min;
+        //     }
+        // }
+
+        // Optimize
+         int cnt=0;
         for(int i=0;i<s.length();i++){
-            Map<Character,Integer> map = new HashMap<>();
+            int[] map= new int[26];
             for(int j=i;j<s.length();j++){
                 char ch = s.charAt(j);
-                map.put(ch,map.getOrDefault(ch,0)+1);
+                map[ch-'a']++;
                 int min=501,max=0;
-                for(int freq: map.values()){
-                    max=Math.max(max,freq);
-                    min = Math.min(min,freq);
+                for(int freq: map){
+                    if(freq>0){
+                        max=Math.max(max,freq);
+                        min = Math.min(min,freq);
+                    }
                 }
                 cnt+=max-min;
             }
