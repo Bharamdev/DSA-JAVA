96f9eae7565881fce05ed13a7dab5735e404861b
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-14 17:50:23 +0530

    Time: 4 ms (39%), Space: 48.1 MB (11.64%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -2,10 +2,12 @@ class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int n=customers.length;
         int res=0;
+        for(int i=0;i<n;i++){
+            if(grumpy[i]==0) res+=customers[i];
+        }
         int max=0,sum=0;
         int r=0,l=0;
         while(r<n){
-             if(grumpy[r]==0) res+=customers[r];
             if(grumpy[r]==1) sum+=customers[r];
             if(r-l>=minutes){
                 if(grumpy[l]==1) sum-=customers[l];
