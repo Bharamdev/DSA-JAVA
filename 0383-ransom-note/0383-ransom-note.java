038ca243ab5ad204e68972287578579be5f2478b
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-17 17:31:44 +0530

    Time: 2 ms (80.07%), Space: 45.2 MB (38.23%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,12 +1,21 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character,Integer> map = new HashMap<>();
-        for(char ch: magazine.toCharArray()) map.put(ch, map.getOrDefault(ch,0)+1);
+        // Map<Character,Integer> map = new HashMap<>();
+        // for(char ch: magazine.toCharArray()) map.put(ch, map.getOrDefault(ch,0)+1);
+        // for(char ch: ransomNote.toCharArray()){
+        //     if(map.getOrDefault(ch,0)>0){
+        //         map.put(ch,map.get(ch)-1);
+        //         if(map.get(ch)==0) map.remove(ch);
+        //     }
+        //     else return false;
+        // }
+        // return true;
+
+        // Optimize 
+        int[] freq = new int[26]; 
+        for(char ch: magazine.toCharArray()) freq[ch-'a']++;
         for(char ch: ransomNote.toCharArray()){
-            if(map.getOrDefault(ch,0)>0){
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==0) map.remove(ch);
-            }
+            if(freq[ch-'a']>0) freq[ch-'a']--;
             else return false;
         }
         return true;
