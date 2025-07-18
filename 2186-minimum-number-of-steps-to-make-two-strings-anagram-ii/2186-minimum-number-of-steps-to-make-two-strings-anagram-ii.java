8fcfe02c868d7d36ed2a5d7ad5fd7b73bb997736
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-18 20:06:31 +0530

    Time: 12 ms (66.8%), Space: 47.4 MB (31.2%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -4,11 +4,10 @@ class Solution {
         for(char ch: s.toCharArray()) freq[ch-'a']++;
         int cnt=0;
         for(char ch: t.toCharArray()){
-            // if(freq[ch-'a']>0) freq[ch-'a']--;
-            // else freq[ch-'a']++;
-            freq[ch - 'a']--;
+            if(freq[ch-'a']>0) freq[ch-'a']--;
+            else cnt++;
         }
-        for(int i=0;i<26;i++) cnt+=Math.abs(freq[i]);
+        for(int i=0;i<26;i++) cnt+=freq[i];
         return cnt;
     }
 }
