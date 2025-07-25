562cff579b2cf25493c2d44f1be5fe5d4d4f89db
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-25 20:59:15 +0530

    Time: 5 ms (36.9%), Space: 57.3 MB (73.42%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -7,8 +7,8 @@ class Solution {
         int diff = 0;
         for(int i=0;i<n;i++){
             rightSum-=(long)nums[i];
-            long rightDiff = Math.abs(leftSum-(i*nums[i]));
-            long leftDiff = Math.abs(rightSum-(nums[i]*(n-1-i)));
+            long rightDiff = Math.abs(rightSum-(nums[i]*(n-1-i)));
+            long leftDiff = Math.abs(leftSum-(i*nums[i]));
             diff = (int)(rightDiff + leftDiff);
             res[i]=diff;
             leftSum+=(long)nums[i];
