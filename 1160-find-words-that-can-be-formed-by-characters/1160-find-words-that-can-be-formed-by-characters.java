5ce3a85917af7e1964a5594f97da2289f34637bf
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-07-29 17:35:54 +0530

    Time: 7 ms (61.58%), Space: 44.9 MB (97.45%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,17 +1,33 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
         int len=0;
-        Map<Character,Integer> map = new HashMap<>();
-        for(char ch : chars.toCharArray()) map.put(ch,map.getOrDefault(ch,0)+1);
+        // Map<Character,Integer> map = new HashMap<>();
+        // for(char ch : chars.toCharArray()) map.put(ch,map.getOrDefault(ch,0)+1);
+        // for(String str: words){
+        //     Map<Character,Integer> copy = new HashMap<>(map);
+        //     boolean good = true;
+        //     for(char ch: str.toCharArray()){
+        //         if(copy.getOrDefault(ch,0)<=0){
+        //             good=false;
+        //             break;
+        //         }
+        //         copy.put(ch,copy.getOrDefault(ch,0)-1);
+        //     }
+        //     if(good) len+=str.length();
+        // }
+
+        // Optimize
+        int[] freq = new int[26];
+        for(char ch: chars.toCharArray()) freq[ch-'a']++;
         for(String str: words){
-            Map<Character,Integer> copy = new HashMap<>(map);
+            int[] curr = new int[26];
             boolean good = true;
             for(char ch: str.toCharArray()){
-                if(copy.getOrDefault(ch,0)<=0){
+                curr[ch-'a']++;
+                if(curr[ch-'a']>freq[ch-'a']){
                     good=false;
                     break;
                 }
-                copy.put(ch,copy.getOrDefault(ch,0)-1);
             }
             if(good) len+=str.length();
         }
