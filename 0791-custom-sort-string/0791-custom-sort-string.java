7b2131da276771818b05b6155f8cf480e2e0badd
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-08-11 18:36:14 +0530

    Time: 0 ms (100%), Space: 41.2 MB (98.03%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,19 +1,41 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        for(char ch: s.toCharArray()) map.put(ch, map.getOrDefault(ch,0)+1);
+        // Map<Character,Integer> map = new HashMap<>();
+        // for(char ch: s.toCharArray()) map.put(ch, map.getOrDefault(ch,0)+1);
+        // StringBuilder str = new StringBuilder();
+        // for(char ch: order.toCharArray()){
+        //     if(map.containsKey(ch)){
+        //         int cnt=map.get(ch);
+        //         for(int i=0;i<cnt;i++){
+        //             str.append(ch);
+        //         }
+        //         map.remove(ch);
+        //     }
+        // }
+        // for(char key: map.keySet()){
+        //     for(int i=0;i<map.get(key);i++) str.append(key);
+        // }
+        // return str.toString();
+
+        // Optimize 
+        int[] freq = new int[26];
+        for(char ch: s.toCharArray()) freq[ch-'a']++;
         StringBuilder str = new StringBuilder();
         for(char ch: order.toCharArray()){
-            if(map.containsKey(ch)){
-                int cnt=map.get(ch);
+            if(freq[ch-'a']>0){
+                int cnt=freq[ch-'a'];
                 for(int i=0;i<cnt;i++){
                     str.append(ch);
+                    freq[ch-'a']--;
                 }
-                map.remove(ch);
             }
         }
-        for(char key: map.keySet()){
-            for(int i=0;i<map.get(key);i++) str.append(key);
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                for(int j=0;j<freq[i];j++){
+                    str.append((char)(i+'a'));
+                }
+            }
         }
         return str.toString();
     }
