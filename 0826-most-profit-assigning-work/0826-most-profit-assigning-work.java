d188bbe91cdbce97e76b0e9b2cc4d3202213cab9
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-08-11 19:11:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        Arrays.sort(worker);
+        int[][] arr = new int[profit.length][2];
+        for(int i=0;i<profit.length;i++){
+            arr[i][0]=difficulty[i];
+            arr[i][1]=profit[i];
+        }
+        Arrays.sort(arr,(a,b) -> a[0]-b[0]);
+        int sum=0;
+        for(int i=0;i<worker.length;i++){
+            int maxProfit=0;
+            for(int p=0;p<arr.length;p++){
+                if(worker[i]>=arr[p][0]) maxProfit = Math.max(maxProfit,arr[p][1]);
+            }
+            if(maxProfit==0) return 0;
+            sum+=maxProfit;
+        }
+        return sum;
+    }
+}
