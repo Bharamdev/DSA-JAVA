4bf32a0300981db6d99a1c9ddf54d4cd844a6b73
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-08-25 00:26:25 +0530

    Time: 21 ms (78.75%), Space: 50.3 MB (86.01%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n=str1.length();
+        int m=str2.length();
+        int[][] dp = LCS(str1, str2, n, m);
+        StringBuilder str = new StringBuilder();
+        int i=n,j=m;
+        while(i>0 && j>0){
+            if(str1.charAt(i-1) == str2.charAt(j-1)){
+                str.insert(0, str1.charAt(i-1));
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i][j-1]>dp[i-1][j]){
+                    str.insert(0, str2.charAt(j-1));
+                    j--;
+                }
+                else{
+                    str.insert(0, str1.charAt(i-1));
+                    i--;
+                }
+            }
+        }
+        while(i>0){
+            str.insert(0, str1.charAt(i-1));
+            i--;
+        }
+        while(j>0){
+            str.insert(0, str2.charAt(j-1));
+            j--;
+        }
+        return str.toString();
+    }
+    public int[][] LCS(String str1, String str2, int n, int m){
+        int[][] dp = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp;
+    }
+}
