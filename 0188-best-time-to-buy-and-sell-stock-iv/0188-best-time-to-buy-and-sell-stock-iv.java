e343da6f8ee60faf103d6c086b05160b0cd85fff
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-09-28 20:18:15 +0530

    Time: 8 ms (31.36%), Space: 45 MB (33.11%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -10,16 +10,18 @@ class Solution {
     int fn(int idx, int buy, int cnt, int[] prices, int k){
         if(idx==prices.length || cnt==k) return 0;
         if(dp[idx][buy][cnt] != -1) return dp[idx][buy][cnt];
+        int max=0;
         if(buy==1){
             int take = -prices[idx] + fn(idx+1, 0, cnt, prices, k);
             int notTake = fn(idx+1, 1, cnt, prices, k);
-            dp[idx][buy][cnt] = Math.max(take, notTake);
+            max = Math.max(take, notTake);
         }
         else{
             int sell = prices[idx] + fn(idx+1, 1, cnt+1, prices, k);
             int notSell = fn(idx+1, 0, cnt, prices, k);
-            dp[idx][buy][cnt] = Math.max(sell, notSell);
+            max = Math.max(sell, notSell);
         }
+        dp[idx][buy][cnt] = max;
         return dp[idx][buy][cnt];
     }
 }
