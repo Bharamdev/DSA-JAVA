9f259c9397b35f3d57c6909a9f55dde021e20934
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-10-06 00:39:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3703-remove-k-balanced-substrings/3703-remove-k-balanced-substrings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeSubstring(String s, int k) {
+        StringBuilder str = new StringBuilder();
+        for(int i=0;i<k;i++) str.append('(');
+        for(int i=0;i<k;i++) str.append(')');
+        StringBuilder ans = new StringBuilder(s);
+        int i=0;
+        while(i<=ans.length()-(2*k)){
+            if(ans.substring(i, i+(2*k)).equals(str.toString())){
+                ans.delete(i, i+(2*k));
+                i = Math.max(0, i-(2*k));
+            }
+            else i++;
+        }
+        return ans.toString();
+    }
+}
