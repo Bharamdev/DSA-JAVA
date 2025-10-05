d9e7aa768c0347781339ccc7f9c76ea0afbee368
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-10-06 00:39:35 +0530

    Time: 2106 ms (100%), Space: 48.2 MB (100%) - LeetHub

--- 3703-remove-k-balanced-substrings/3703-remove-k-balanced-substrings.java
+++ 3703-remove-k-balanced-substrings/3703-remove-k-balanced-substrings.java
@@ -4,9 +4,10 @@ class Solution {
         for(int i=0;i<k;i++) str.append('(');
         for(int i=0;i<k;i++) str.append(')');
         StringBuilder ans = new StringBuilder(s);
+        String pattern = str.toString();
         int i=0;
         while(i<=ans.length()-(2*k)){
-            if(ans.substring(i, i+(2*k)).equals(str.toString())){
+            if(ans.substring(i, i+(2*k)).equals(pattern)){
                 ans.delete(i, i+(2*k));
                 i = Math.max(0, i-(2*k));
             }
