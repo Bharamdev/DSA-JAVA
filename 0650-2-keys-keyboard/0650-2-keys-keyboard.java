dcc31f09586df1ce0a491864c2b744d9b97cb4f8
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-10-09 23:18:09 +0530

    Time: 19 ms (21.58%), Space: 54.8 MB (15.73%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,13 +1,17 @@
 class Solution {
+    int[][] dp;
     public int minSteps(int n) {
         if(n==1) return 0;
+        dp = new int[n][n];
+        for(int[] a: dp) Arrays.fill(a,-1);
         return 1 + fn(1,1,n);
     }
     int fn(int current,int prev, int n){
         if(current==n) return 0;
         if(current>n) return Integer.MAX_VALUE/2;
+        if(dp[current][prev] != -1) return dp[current][prev];
         int copyPaste = 2 + fn(current*2, current, n);
         int paste = 1 + fn(current+prev, prev, n);
-        return Math.min(paste, copyPaste);
+        return dp[current][prev] = Math.min(paste, copyPaste);
     }
 }
