e1dfb1f0ea790d144cce877d4b93bbb68ecd90fa
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-10-10 18:56:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -12,29 +12,37 @@ class Node{
 */
 
 class Solution {
+    static class pair{
+        Node node;
+        int pos;
+        pair(Node node, int pos){
+            this.node = node;
+            this.pos=pos;
+        }
+    }
     // Function to return a list of nodes visible from the top view
     // from left to right in Binary Tree.
     static ArrayList<Integer> topView(Node root) {
         // code here
         if(root==null) return null;
         Map<Integer,Integer> map = new TreeMap<>();
-        Queue<Node> q = new LinkedList<>();
-        Queue<Integer> n = new LinkedList<>();
-        q.offer(root);
-        n.offer(0);
+        Queue<pair> q =new LinkedList<>();
+        q.offer(new pair(root,0));
+        // Queue<Node> q = new LinkedList<>();
+        // Queue<Integer> n = new LinkedList<>();
+        // q.offer(root);
+        // n.offer(0);
         while(!q.isEmpty()){
-            Node curr=q.poll();
-            int num = n.poll();
+            Node curr=q.peek().node;
+            int num = q.poll().pos;
             if(!map.containsKey(num)){
                 map.put(num,curr.data);
             }
             if(curr.left!=null){
-                q.offer(curr.left);
-                n.offer(num-1);
+                q.offer(new pair(curr.left, num-1));
             }
             if(curr.right!=null){
-                q.offer(curr.right);
-                n.offer(num+1);
+                q.offer(new pair(curr.right, num+1));
             }
         }
         ArrayList<Integer> arr = new ArrayList<>(map.values());
