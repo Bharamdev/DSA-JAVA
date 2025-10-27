f5ff49b0c732b5c0a83fd67e8a7f6a5fe2955e1b
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-10-27 10:32:15 +0530

    Time: 1 ms (50.32%), Space: 40.8 MB (26.31%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -4,7 +4,13 @@ class Solution {
         int t=n;
         for(int i=0; i<=n/7; i++){
             int k=i+1;
-            for(int j=0;j<Math.min(t,7);j++) total+=k++;
+            // for(int j=0;j<Math.min(t,7);j++) total+=k++;
+            if(t<7){
+                for(int j=0;j<t;j++) total+=k++;
+            }
+            else{
+                for(int j=0;j<7;j++) total+=k++;
+            }
             t-=7;
         }
         return total;
