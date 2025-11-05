d75ea83a1800e4b25b353ddea870e9e09dc17b79
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-11-06 01:50:33 +0530

    Time: 356 ms (33.57%), Space: 47.9 MB (6.43%) - LeetHub

--- 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.java
+++ 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.java
@@ -1,35 +1,34 @@
 class Solution {
-    int ans;
-    ArrayList<ArrayList<Integer>> adj;
-     int[] depth;
     public int findShortestCycle(int n, int[][] edges) {
-        adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
         for (int[] e : edges) {
             adj.get(e[0]).add(e[1]);
             adj.get(e[1]).add(e[0]);
         }
-        boolean[] vis = new boolean[n];
-        int[] parent = new int[n];
-        depth = new int[n];
-        Arrays.fill(parent,-1);
-        ans=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            if(!vis[i]) dfs(i,0,vis,parent);
+        int ans = Integer.MAX_VALUE;
+        for(int s=0;s<n;s++){
+            Queue<Integer> q = new LinkedList<>();
+            int[] dist = new int[n];
+            Arrays.fill(dist, -1);
+            int[] parent = new int[n];
+            Arrays.fill(parent, -1);
+            q.offer(s);
+            dist[s]=0;
+            while(!q.isEmpty()){
+                int node = q.poll();
+                for(int it: adj.get(node)){
+                    if(dist[it]==-1){
+                        dist[it]=dist[node]+1;
+                        parent[it]=node;
+                        q.offer(it);
+                    }
+                    else if(parent[node]!=it){
+                        ans = Math.min(ans, dist[node]+dist[it]+1);
+                    }
+                }
+            }
         }
         return ans==Integer.MAX_VALUE ? -1 : ans;
-    }
-    void dfs(int node, int d, boolean[] vis, int[] parent){
-        vis[node]=true;
-        depth[node]=d;
-        for(int it: adj.get(node)){
-            if(!vis[it]){
-                parent[it]=node;
-                dfs(it, d+1, vis, parent);
-            }
-            else if(parent[node]!=it){
-                ans = Math.min(ans, Math.abs(depth[node]-depth[it])+1);
-            }
-        }
     }
 }
