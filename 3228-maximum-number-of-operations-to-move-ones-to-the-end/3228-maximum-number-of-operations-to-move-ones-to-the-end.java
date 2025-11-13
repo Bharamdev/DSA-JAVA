1f8b0787b8beac964a9058d9fbe02fba1f8b1885
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-11-13 12:44:48 +0530

    Time: 7 ms (78.52%), Space: 47.8 MB (7.41%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -2,9 +2,21 @@ class Solution {
     public int maxOperations(String s) {
         int one=0;
         int cnt=0;
-        for(int i=0;i<s.length()-1;i++){
-            if(s.charAt(i)=='1') one++;
-            if(s.charAt(i)=='1' && s.charAt(i+1)=='0') cnt+=one;
+        // for(int i=0;i<s.length()-1;i++){
+        //     if(s.charAt(i)=='1') one++;
+        //     if(s.charAt(i)=='1' && s.charAt(i+1)=='0') cnt+=one;
+        // }
+
+        // optimize
+        int n = s.length();
+        int i=0;
+        while(i<n){
+            if(s.charAt(i)=='0'){
+                while(i+1<n && s.charAt(i+1)=='0') i++;
+                cnt+=one;
+            }
+            else one++;
+            i++;
         }
         return cnt;
     }
