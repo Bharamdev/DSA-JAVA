a9ed124b4e19699ab44e08f611b200a2880f9f46
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-11-19 19:07:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+class Solution {
+    public long countFriendsPairings(int n) {
+        // code here
+        if(n<=2) return n;
+        long[] dp = new long[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + (long)(i-1) * dp[i-2];
+        }
+        return dp[n];
+    }
+}
