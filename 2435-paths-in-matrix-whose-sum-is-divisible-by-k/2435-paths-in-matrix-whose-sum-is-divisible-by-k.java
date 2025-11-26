97ac09c8b78a4f7e6e651587cce5fa6e9bd1f352
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-11-27 00:37:41 +0530

    Time: 47 ms (84.23%), Space: 102.5 MB (16.66%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -11,6 +11,8 @@ class Solution {
         //     for(int[] d: arr) Arrays.fill(d, -1);
         // }
         // return fn(0,0,0,grid,k);
+
+        // Tabulation
         dp = new int[n+1][m+1][k+1];
         for(int rem=0;rem<k;rem++){
             dp[n-1][m-1][rem] = (rem+grid[n-1][m-1])%k==0 ? 1 : 0;
@@ -28,6 +30,8 @@ class Solution {
         }
         return dp[0][0][0];
     }
+
+    // Memo
     // int fn(int i, int j, int sum, int[][] grid, int k){
     //     if(i>=n|| j>=m) return 0;
     //     if(i==n-1 && j==m-1){
