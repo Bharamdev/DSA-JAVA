cb4580f9488d38364c622a14d569e952a5f83fb3
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-12-02 17:40:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.java
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.java
@@ -3,30 +3,30 @@ class Solution {
     public ArrayList<ArrayList<Integer>> nQueen(int n) {
         // code here
         ans = new ArrayList<>();
-        boolean[][] q = new boolean[n][n];
+        // boolean[][] q = new boolean[n][n];
         boolean[] arr = new boolean[n];
         boolean[] rdig = new boolean[2*n-1];
         boolean[] ldig = new boolean[2*n-1];
         ArrayList<Integer> list = new ArrayList<>();
-        solve(0, q, list, arr, rdig, ldig);
+        solve(0,n, list, arr, rdig, ldig);
         return ans;
     }
-    void solve(int row, boolean[][] q, ArrayList<Integer> list, 
+    void solve(int row, int n, ArrayList<Integer> list, 
                boolean[] arr, boolean[] rdig, boolean[] ldig){
-        if(row==q.length){
+        if(row==n){
             ans.add(new ArrayList<>(list));
             return;
         }
-        for(int col=0;col<q.length;col++){
-            if(!arr[col] && !rdig[col+row] && !ldig[q.length-1+col-row]){
+        for(int col=0;col<arr.length;col++){
+            if(!arr[col] && !rdig[col+row] && !ldig[n-1+col-row]){
                 arr[col]=true;
                 rdig[col+row]=true;
-                ldig[q.length-1+col-row]=true;
+                ldig[n-1+col-row]=true;
                 list.add(col+1);
-                solve(row+1, q, list, arr, rdig, ldig);
+                solve(row+1, n, list, arr, rdig, ldig);
                 arr[col]=false;
                 rdig[col+row]=false;
-                ldig[q.length-1+col-row]=false;
+                ldig[n-1+col-row]=false;
                 list.remove(list.size()-1);
             }
         }       
