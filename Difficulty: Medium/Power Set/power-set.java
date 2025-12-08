b6cdc18282dfa538df31391b11a4137a6f2a4484
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-12-08 13:59:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Power Set/power-set.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public List<String> AllPossibleStrings(String s) {
+        // Code here
+        List<String> res = new ArrayList<>();
+        int n=s.length();
+        int subsets = 1<<n;
+        for(int num=1;num<subsets;num++){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<n;i++){
+                if((num&(1<<i))!=0) sb.append(s.charAt(i));
+            }
+            res.add(sb.toString());
+        }
+        Collections.sort(res);
+        return res;
+    }
+}
