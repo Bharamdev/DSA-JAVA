93f2ce7c42e77b1200c62bb3067dcbc85b55eb15
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2025-12-22 11:39:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        int[] inDegree = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int[] e: edges){
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+            inDegree[e[0]]++;
+            inDegree[e[1]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(inDegree[i]==1) q.offer(i);
+        }
+        int remaining = n;
+        while(remaining>2){
+            int size = q.size();
+            remaining-=size;
+            for(int leaf=0; leaf<size; leaf++){
+                int node = q.poll();
+                for(int it: adj.get(node)){
+                    inDegree[it]--;
+                    if(inDegree[it]==1) q.offer(it);
+                }
+            }
+        }
+        return new ArrayList<>(q);
+    }
+}
