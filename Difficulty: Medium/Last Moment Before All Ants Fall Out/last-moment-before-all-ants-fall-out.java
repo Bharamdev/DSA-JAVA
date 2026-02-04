44a88dba30c0a9e82a647ed60a9741a0f460de6f
Bharam Dev <157497417+Bharamdev@users.noreply.github.com>
2026-02-04 23:50:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int getLastMoment(int n, int left[], int right[]) {
+        // code here
+        int ans=0;
+        for(int l: left) ans = Math.max(ans,l);
+        for(int r: right) ans = Math.max(ans,n-r);
+        return ans;
+    }
+}
